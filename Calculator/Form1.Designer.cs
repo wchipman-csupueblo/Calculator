bbb44a9d98178970ea1d89372b237cf17ae24b2c
bill chipman <wrchipman@gmail.com>
2023-09-14 21:32:49 -0400

    Completed all number button inputs

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -58,6 +58,7 @@
             this.Num0.TabIndex = 10;
             this.Num0.Text = "0";
             this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
             // 
             // Dot
             // 
@@ -67,6 +68,7 @@
             this.Dot.TabIndex = 11;
             this.Dot.Text = ".";
             this.Dot.UseVisualStyleBackColor = true;
+            this.Dot.Click += new System.EventHandler(this.Dot_Click);
             // 
             // Cbut
             // 
@@ -94,6 +96,7 @@
             this.Num7.TabIndex = 7;
             this.Num7.Text = "7";
             this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
             // 
             // Num8
             // 
@@ -103,6 +106,7 @@
             this.Num8.TabIndex = 8;
             this.Num8.Text = "8";
             this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
             // 
             // Num9
             // 
@@ -112,6 +116,7 @@
             this.Num9.TabIndex = 9;
             this.Num9.Text = "9";
             this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
             // 
             // Pow
             // 
@@ -157,6 +162,7 @@
             this.Num6.TabIndex = 6;
             this.Num6.Text = "6";
             this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
             // 
             // Num5
             // 
@@ -166,6 +172,7 @@
             this.Num5.TabIndex = 5;
             this.Num5.Text = "5";
             this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
             // 
             // Num4
             // 
@@ -175,6 +182,7 @@
             this.Num4.TabIndex = 4;
             this.Num4.Text = "4";
             this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
             // 
             // Div
             // 
@@ -202,6 +210,7 @@
             this.Num3.TabIndex = 3;
             this.Num3.Text = "3";
             this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
             // 
             // Num2
             // 
@@ -211,6 +220,7 @@
             this.Num2.TabIndex = 2;
             this.Num2.Text = "2";
             this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
             // 
             // Num1
             // 
@@ -220,6 +230,7 @@
             this.Num1.TabIndex = 1;
             this.Num1.Text = "1";
             this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
             // 
             // Output
             // 
